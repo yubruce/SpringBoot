e30bbf82ee81d43c83c90db535517affa2d6d3d6
yubruce <jush.lu@gmail.com>
2025-09-28 13:26:30 +0800

    Add files via upload

---
+++ SpringBoot3-ActuatorSecurity/src/main/java/org/example/springboot3actuatorsecurity/SpringBoot3ActuatorSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.example.springboot3actuatorsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3ActuatorSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3ActuatorSecurityApplication.class, args);
+    }
+
+}
---
+++ SpringBoot3-ActuatorSecurity/src/test/java/org/example/springboot3actuatorsecurity/SpringBoot3ActuatorSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springboot3actuatorsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3ActuatorSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
