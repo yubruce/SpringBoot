6465c96b3cf07c9cbfc32a5f76ab559e346d9be2
yubruce <jush.lu@gmail.com>
2025-09-30 13:45:12 +0800

    Add files via upload
    init

---
+++ SpringBoot3_PropertirsValues/src/main/java/com/example/springboot3_provalues/SpringBoot3PropertirsValuesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot3_provalues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3PropertirsValuesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3PropertirsValuesApplication.class, args);
+    }
+
+}
---
+++ SpringBoot3_PropertirsValues/src/test/java/com/example/springboot3_provalues/SpringBoot3PropertirsValuesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot3_provalues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3PropertirsValuesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
